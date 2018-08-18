801a1abb1d3d9aec052302ca4e16f18964800192
Maxence Roy <m@masserm.com>
2018-08-18 15:35:43 -0500

    Initial Commit

---
+++ pulse/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pulse
+//
+//  Created by Maxence Roy on 8/18/18.
+//  Copyright © 2018 Health Guardian Limited. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
